b425de2ade0e6f34e32739b0cd398b3847e2c2d9
Jason Sonnichsen <Harliquinn@gmail.com>
2020-01-27 16:52:14 -0500

    Tried to get the DLL to import from correct dir Didn't work. Answered today's questions.

--- queue_and_stack/doubly_linked_list.py
+++ queue_and_stack/doubly_linked_list.py
@@ -47,8 +47,8 @@ class DoublyLinkedList:
 
     def __len__(self):
         return self.length
-    """Wraps the given value in a ListNode and inserts it 
-    as the new head of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new head of the list. Don't forget to handle
     the old head node's previous pointer accordingly."""
 
     def add_to_head(self, value):
@@ -69,8 +69,8 @@ class DoublyLinkedList:
         value = self.head.value
         self.delete(self.head)
         return value
-    """Wraps the given value in a ListNode and inserts it 
-    as the new tail of the list. Don't forget to handle 
+    """Wraps the given value in a ListNode and inserts it
+    as the new tail of the list. Don't forget to handle
     the old tail node's next pointer accordingly."""
 
     def add_to_tail(self, value):
@@ -83,7 +83,7 @@ class DoublyLinkedList:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
-    """Removes the List's current tail node, making the 
+    """Removes the List's current tail node, making the
     current tail's previous node the new tail of the List.
     Returns the value of the removed Node."""
 
@@ -91,14 +91,14 @@ class DoublyLinkedList:
         value = self.tail.value
         self.delete(self.tail)
         return value
-    """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new head node of the List."""
 
     def move_to_front(self, node):
         value = node.value
         self.delete(node)
         self.add_to_head(value)
-    """Removes the input node from its current spot in the 
+    """Removes the input node from its current spot in the
     List and inserts it as the new tail node of the List."""
 
     def move_to_end(self, node):
